e1c7e3e58e223a589e306d008d7c0032146b7fbf
johnwav <102820657+johnwav@users.noreply.github.com>
2023-06-12 21:57:44 +0100

    feat: create new rooms and  change room status

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 import dbConnect from "@/utils/database";
 import Room from "@/models/Rooms";
 
@@ -9,8 +9,26 @@ export async function GET(request: Request) {
       { $match: { status: "waiting" } },
       { $sample: { size: 1 } },
     ]);
+    if (rooms.length > 0) {
+      const roomId = rooms[0]._id;
+      await Room.findByIdAndUpdate(roomId, {
+        status: "chatting",
+      });
+    }
     return new Response(JSON.stringify(rooms), { status: 200 });
   } catch (error) {
     return new Response(error as any, { status: 400 });
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const room = await Room.create({
+      status: "waiting",
+    });
+    return new Response(JSON.stringify(room), { status: 200 });
+  } catch (error) {
+    return new Response("failed to create new room", { status: 500 });
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
-import styles from './page.module.css'
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
+
+type Room = {
+  _id: string;
+  status: string;
+};
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-        hey
-    </main>
-  )
+  const [room, setRoom] = useState<Room | undefined>();
+  const fetchRooms = async () => {
+    const response = await fetch("/api/rooms");
+    const rooms = await response.json();
+    // debugger;
+    if (rooms.length > 0) {
+      // TODO: connect to the room
+      setRoom(rooms[0]);
+    } else {
+      // create a new room
+      const response = await fetch("/api/rooms", {
+        method: "POST",
+      });
+      const room = await response.json();
+      setRoom(room);
+    }
+  };
+
+  useEffect(() => {
+    fetchRooms();
+  }, []);
+
+  return <main className={styles.main}>{room?._id}</main>;
 }
