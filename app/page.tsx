b8017b085343b40d34cf0ce80c1c6a2df1d21536
johnwav <102820657+johnwav@users.noreply.github.com>
2023-06-13 01:16:14 +0100

    agora init

--- app/page.tsx
+++ app/page.tsx
@@ -7,28 +7,67 @@ type Room = {
   status: string;
 };
 
+type Tmessage = {
+  userId: string;
+  message: string;
+};
+
+async function connectToAgora(roomId: string, userId: string) {
+  const { default: AgoraRTM } = await import("agora-rtm-sdk");
+  const client = AgoraRTM.createInstance(process.env.NEXT_PUBLIC_AGORA_APP_ID!);
+  await client.login({
+    uid: userId,
+  });
+  const channel = await client.createChannel(roomId);
+  await channel.join();
+  channel.on("ChannelMessage", (message, peerId) => {
+    console.log(message, peerId);
+  });
+}
+
+async function getRandomRoom(): Promise<Room[]> {
+  const response = await fetch("/api/rooms");
+  const rooms = await response.json();
+  return rooms;
+}
+
+async function createRoom(): Promise<Room> {
+  const response = await fetch("/api/rooms", {
+    method: "POST",
+  });
+  const room = await response.json();
+  return room;
+}
+
 export default function Home() {
   const [room, setRoom] = useState<Room | undefined>();
-  const fetchRooms = async () => {
-    const response = await fetch("/api/rooms");
-    const rooms = await response.json();
-    // debugger;
-    if (rooms.length > 0) {
-      // TODO: connect to the room
-      setRoom(rooms[0]);
-    } else {
-      // create a new room
-      const response = await fetch("/api/rooms", {
-        method: "POST",
-      });
-      const room = await response.json();
-      setRoom(room);
-    }
-  };
+  const [userId, setuserId] = useState(Math.random() * 1e6 + "");
+  const [message, setmessage] = useState([]);
 
-  useEffect(() => {
+  async function fetchRooms() {
+    getRandomRoom().then((rooms) => {
+      if (rooms.length > 0) {
+        setRoom(rooms[0]);
+        connectToAgora(rooms[0]._id, userId);
+      } else {
+        createRoom().then((room) => {
+          setRoom(room);
+          connectToAgora(room?._id, userId);
+        });
+      }
+    });
+  }
+
+  function handleStartChat() {
     fetchRooms();
-  }, []);
+  }
 
-  return <main className={styles.main}>{room?._id}</main>;
+  useEffect(() => {}, []);
+
+  return (
+    <main className={styles.main}>
+      <button onClick={handleStartChat}>Chat</button>
+      {room?._id}
+    </main>
+  );
 }
