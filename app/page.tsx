d093388e678088a7852bf773d69d0ef3e62077e2
johnwav <102820657+johnwav@users.noreply.github.com>
2023-06-13 23:38:09 +0100

    chat room working

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./page.module.css";
+import { RtmChannel } from "agora-rtm-sdk";
 
 type Room = {
   _id: string;
@@ -29,8 +30,11 @@ async function connectToAgora(
       userId,
       message: message.text,
     });
-    // console.log(message, peerId);
   });
+
+  return {
+    channel,
+  };
 }
 
 async function getRandomRoom(): Promise<Room[]> {
@@ -50,36 +54,62 @@ async function createRoom(): Promise<Room> {
 export default function Home() {
   const [room, setRoom] = useState<Room | undefined>();
   const [userId, setuserId] = useState(Math.random() * 1e6 + "");
-  const [message, setMessages] = useState<Tmessage[]>([]);
+  const [messages, setMessages] = useState<Tmessage[]>([]);
+  const [input, setInput] = useState("");
+  const channelRef = useRef<RtmChannel>();
+
+  async function handleSendMessage() {
+    await channelRef.current?.sendMessage({
+      text: input,
+    });
+    setInput("");
+  }
 
   async function fetchRooms() {
-    getRandomRoom().then((rooms) => {
-      if (rooms && rooms.length > 0) {
-        setRoom(rooms[0]);
-        connectToAgora(rooms[0]?._id, userId, (message: Tmessage) =>
-          setMessages((curr) => [...curr, message])
-        );
-      } else {
-        createRoom().then((room) => {
-          setRoom(room);
-          connectToAgora(room._id, userId, (message: Tmessage) =>
-            setMessages((curr) => [...curr, message])
-          );
-        });
-      }
-    });
+    const rooms = await getRandomRoom();
+    if (rooms && rooms.length > 0) {
+      setRoom(rooms[0]);
+      const { channel } = await connectToAgora(
+        rooms[0]?._id,
+        userId,
+        (message: Tmessage) => setMessages((curr) => [...curr, message])
+      );
+      channelRef.current = channel;
+    } else {
+      const room = await createRoom();
+      setRoom(room);
+      const { channel } = await connectToAgora(
+        room._id,
+        userId,
+        (message: Tmessage) => setMessages((curr) => [...curr, message])
+      );
+      channelRef.current = channel;
+    }
   }
 
   function handleStartChat() {
     fetchRooms();
   }
 
-  useEffect(() => {}, []);
-
   return (
     <main className={styles.main}>
-      <button onClick={handleStartChat}>Chat</button>
       {room?._id}
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>
+            {message?.userId} {message?.message}
+          </li>
+        ))}
+      </ul>
+
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+      />
+      <button onClick={handleSendMessage}>send</button>
+
+      <button onClick={handleStartChat}>Chat</button>
     </main>
   );
 }
