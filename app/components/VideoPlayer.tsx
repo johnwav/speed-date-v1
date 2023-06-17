154d395b18cf330c9144526c0ee5fba88627dd5a
johnwav <102820657+johnwav@users.noreply.github.com>
2023-06-18 00:50:06 +0100

    agora vido and audio connect

---
+++ app/components/VideoPlayer.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from "react";
+import { ICameraVideoTrack, IRemoteVideoTrack } from "agora-rtc-sdk-ng";
+
+
+export const VideoPlayer = ({
+    videoTrack,
+    style,
+  }: {
+    videoTrack: IRemoteVideoTrack | ICameraVideoTrack
+    style: object;
+  }) => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+      const playerRef = ref.current;
+      if (!videoTrack) return;
+      if (!playerRef) return;
+
+      videoTrack.play(playerRef);
+
+      return () => {
+        videoTrack.stop();
+      };
+    }, [videoTrack]);
+
+    return <div ref={ref} style={style}></div>;
+  };
